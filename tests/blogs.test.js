137a424036b56033f4933cd1467de585ba11eae6
Josseph Morales <johnymoore852@gmail.com>
2023-02-19 13:56:22 -0600

    4.8: Blog list tests, step1

---
+++ tests/blogs.test.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://www.acm.org/classics/sep95/',
+    likes: 5,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let noteObject = new Blog(initialBlogs[0])
+  await noteObject.save()
+  noteObject = new Blog(initialBlogs[1])
+  await noteObject.save()
+})
+
+test('correct amount of blog posts in the JSON format', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+}, 100000)
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
